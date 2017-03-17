351892c225596f278407ade6225d0a713d3e4930
Puja Goyal <puja.goyal@tothenew.com>
2017-03-17 15:47:11 +0530

    Changes done in birthDate

--- nodejs/ques4_util.js
+++ nodejs/ques4_util.js
@@ -1,18 +1,23 @@
 function getAge(birthDate) {
                 var today=new Date();
                 var birthDate1= new Date(birthDate);
-                if(birthDate1!="Invalid Date"){
-                                             
+         if(birthDate1!="Invalid Date"){
+               if((today-birthDate1)<1 || (today.getDate()==birthDate1.getDate())&&(today.getMonth()==birthDate1.getMonth())&&(today.getFullYear()==birthDate1.getFullYear() )){
+                            console.log("Invalid input");
+                                 return 0;
+                                   }else{
     		var age = today.getFullYear() - birthDate1.getFullYear();
                 var m = today.getMonth() - birthDate1.getMonth();
                   if( m < 0 || (m == 0 && today.getDate() < birthDate1.getDate())) {
                        age--;
-}
+                                   }
           return age;
-}
+                             }
+   }
 else{
 
-    console.log("invalid date ,Correct date format is (YYYY/MM/DD)");   
+    console.log("invalid date ,Correct date format is (YYYY/MM/DD)");
+    return 0;   
 }
 }
 module.exports.getAge =getAge; 
